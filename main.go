4bcc1d270471aa2843c53f2d32c95f6e966f8516
danilovict2 <danilosindric@gmail.com>
2024-08-27 15:35:07 +0200

    Add custom handler

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 
 	server := http.Server{
 		Handler: mux,
