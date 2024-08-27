fc478d6a021f15b3f4d7c2994b09ca5a3f627964
danilovict2 <danilosindric@gmail.com>
2024-08-27 16:13:31 +0200

    moved non-website endpoints to the /api namespace

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := apiConfig{}
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.Handle("GET /app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	
-	mux.HandleFunc("/healthz", func (w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func (w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
 
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 
 
 	server := http.Server{
